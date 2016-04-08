a48cc1cd3560c45ef0cf92abaf7a8077781a8410
rilian <dmitriyis@gmail.com>
2016-04-08 16:24:22 +0300

    reorganize configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,13 +34,13 @@ Errbit::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
